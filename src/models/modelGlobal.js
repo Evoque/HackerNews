200691ef9ab599dba564d1a9b351932b43f61d20
Evoque <rubievoque@gmail.com>
2019-04-19 15:09:06 +0800

    add & test firebase

--- src/models/modelGlobal.js
+++ src/models/modelGlobal.js
@@ -1,6 +1,34 @@
 
 
 import * as serviceGlobal from './../services/serviceGlogal';
+import axios from 'axios';
+import Firebase from 'firebase/app';
+import 'firebase/database';
+
+function createAPI({config, version}) {
+  Firebase.initializeApp(config)
+  return Firebase.database().ref(version)
+}
+
+
+const api = createAPI({
+  version: '/v0',
+  config: {
+    databaseURL: 'https://hacker-news.firebaseio.com'
+  }
+})
+
+function fetch(child) {
+  console.log(`fetching ${child}...`)
+  return new Promise((resolve, reject) => {
+    api.child(child).once('value', snapshot => {
+      const val = snapshot.val()
+      // mark the timestamp when this item is cached
+      if (val) val.__lastUpdated = Date.now();
+      resolve(val)
+    }, reject)
+  })
+}
 
 export default {
 
@@ -11,7 +39,20 @@ export default {
   subscriptions: {
     setup({dispatch, history}) {
       history.listen(() => {
-        dispatch({type: 'QUERY_LIST', payload: {type: 'top'}});
+
+        // axios({
+        //   method: 'get',
+        //   url: 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty',
+        //   responseType: 'application/json'
+        // }).then(resp => {
+        //   console.log(resp);
+        // }) 
+        fetch('topstories')
+          .then(a => {
+            console.log(a);
+          })
+
+
       });
     },
   },
@@ -21,6 +62,7 @@ export default {
     //   yield put({ type: 'save' });
     // },
     *QUERY_LIST({payload: {type}}, {call}) {
+      console.log('wh')
       const result = yield call(serviceGlobal.fetchStoriesByType, type);
       console.log(result);
     }
